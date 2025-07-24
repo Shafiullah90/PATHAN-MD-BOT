73063076d10cc783d5567cabe468e23cf04eb751
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-07-24 09:52:30 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: 'supreme',
   botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
   botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  ownerNumber: '66620925025', //Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
