7a7d73848bac47c449d56613c8cfccb1654f5c17
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-07-24 14:28:35 +0430

    Update alive.js

--- commands/alive.js
+++ commands/alive.js
@@ -20,7 +20,7 @@ async function aliveCommand(sock, chatId, message) {
                 isForwarded: false,
                 forwardedNewsletterMessageInfo: {
                     newsletterJid: '@newsletter',
-                    newsletterName: '𝐉ᴜɴᴇ 𝐌ᴅ',
+                    newsletterName: 'Shafi bot',
                     serverMessageId: -1
                 }
             }
