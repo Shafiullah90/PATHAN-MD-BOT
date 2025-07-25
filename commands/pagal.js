4c0c5471694d086ea9b215f521717e4c38f97ae1
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-07-25 20:40:30 +0430

    Delete commands/pagal.js

--- commands/pagal.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  command: 'pagal',
-  handler: async (sock, chatId, message) => {
-    await sock.sendMessage(chatId, { text: 'when you smile, it feels like everything will be alright. Your crazy lover ' });
-  }
-};
-```
