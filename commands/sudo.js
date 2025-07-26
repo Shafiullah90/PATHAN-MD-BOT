4eac5b022cfcc5ff3453a0da348fb41d05c72f68
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-07-26 16:57:54 +0430

    Update sudo.js

--- commands/sudo.js
+++ commands/sudo.js
@@ -1,17 +1,15 @@
+const isOwner = require('../lib/isOwner');
+const { addSudoUser } = require('../lib/sudomanager');
+
 async function sudoCommand(sock, chatId, message, args) {
-  try {
-    if (args[0] === 'add') {
-      const sudoUser = message.quoted ? message.quoted.sender : message.mentionedJid[0];
-      if (sudoUser && isOwner(message.sender)) {
-        addSudoUser(sudoUser);
-        await sock.sendMessage(chatId, { text: `Added @${sudoUser.split('@')[0]} as sudo user!`, ...channelInfo, mentions: [sudoUser] });
-      } else {
-        await sock.sendMessage(chatId, { text: 'Only owners can add sudo users!', ...channelInfo });
-      }
+  if (args[0] === 'add') {
+    const sudoUser = message.quoted ? message.quoted.sender : message.mentionedJid[0];
+    if (sudoUser && isOwner(message.sender)) {
+      addSudoUser(sudoUser);
+      await sock.sendMessage(chatId, { text: `Added @${sudoUser.split('@')[0]} as sudo user!`, mentions: [sudoUser] });
+    } else {
+      await sock.sendMessage(chatId, { text: 'Only owners can add sudo users!' });
     }
-  } catch (error) {
-    console.error('Error in sudo command:', error);
-    await sock.sendMessage(chatId, { text: '❌ Failed to execute sudo command. Please try again.' }, { quoted: message });
   }
 }
 
