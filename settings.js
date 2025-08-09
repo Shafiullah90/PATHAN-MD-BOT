86cd600b81989a54962839653e4ad71d2ebe0e3c
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-08-09 19:11:49 +0430

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,13 @@
+const settings = {
+  packname: '✨PATHAN BOT✨',
+  author: '👑 Shafi Hacks',
+  botName: '🤖 PATHAN BOT',
+  botOwner: 'Shafi Hacks', // Your name
+  ownerNumber: '93703802176', // Set your number here without + symbol.
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: 'public', // can be 'public' or 'private'
+  description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by pathan Hacks.',
+  version: '2.0.0',
+};
+
+module.exports = settings;
