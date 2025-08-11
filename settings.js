efd82a94a2986763a9047287b0e6195cdfd7cdf9
Shafiullah90 <shafiullahpathan704@gmail.com>
2025-08-11 07:59:22 +0430

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,12 +1,12 @@
 const settings = {
-  packname: '✨PATHAN BOT✨',
-  author: '👑 Shafi Hacks',
+  packname: '✨PATHAN-BOT✨',
+  author: '👑 Shafi PATHAN🇦🇫',
   botName: '🤖 PATHAN BOT',
-  botOwner: 'Shafi Hacks', // Your name
+  botOwner: 'Shafi PATHAN🇦🇫', // Your name here u can change 
   ownerNumber: '93703802176', // Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: 'public', // can be 'public' or 'private'
-  description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by pathan Hacks.',
+  description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by pathan power.',
   version: '2.0.0',
 };
 
